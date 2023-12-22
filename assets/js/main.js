795154e8400b70080fcd624a52553ed46dde2d79
João Vitor Moreira da Silva <110782871+silva-moreira@users.noreply.github.com>
2023-12-22 12:36:44 -0300

    att 0.3 BETA

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,3 +133,18 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/* ================ SCROLL SECTIONS ACTIVE LINK ================ */ 
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });SS
